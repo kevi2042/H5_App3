c6aa7573a53a73529d9caa2511e877b8f0f7803a
Kevin Wolff <kevi2042@elevcampus.dk>
2021-11-10 11:29:56 +0100

    tilføjet opgave 6.2 til github

---
+++ ConsoleHttpClient/ConsoleHttpClient/IBackendService.cs
@@ -0,0 +1,27 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHttpClient
+{
+    public interface IBackendService
+    {
+        [Get("/api/items")]
+        Task<List<Item>> GetItems();
+
+        [Get("/api/items/{id}")]
+        Task<Item> GetItemById(string id);
+
+        [Post("/api/items")]
+        Task AddItem([Body] Item item);
+
+        [Put("api/items/{id}")]
+        Task UpdateItem(string id, [Body] Item item);
+
+        [Delete("api/items/{id}")]
+        Task Delete(string id);
+    }
+}
---
+++ ConsoleHttpClient/ConsoleHttpClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Refit;
+using System;
+
+
+namespace ConsoleHttpClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // Her oprettes et objekt af IBackendService-typen
+            var repos = RestService.For<IBackendService>("https://localhost:5001");
+
+            Item newItem = new Item { Text = "New Item", Description = "My New Item" };
+            await repos.AddItem(newItem);
+
+            var items = await repos.GetItems();
+            foreach (Item item in items)
+            {
+                Console.WriteLine($"Text: {item.Text} - Description: {item.Description}");
+            }
+
+            Item singleItem = await repos.GetItemById(items[5].Id);
+            Console.WriteLine($"\nSingle Item: {singleItem.Text} - Description: {singleItem.Description}");
+             
+            singleItem.Description = "This is a new Description";
+            await repos.UpdateItem("5", singleItem);
+            singleItem = await repos.GetItemById(items[5].Id);
+            Console.WriteLine($"\nUpdated Single Item: {singleItem.Text} - Description: {singleItem.Description}");
+
+            Console.ReadLine();
+        }
+    }
+}
